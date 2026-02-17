14883a61d4542bc5e78a04cc635e5ad85d11625c
eduardo-zucco <eduardozucco89@gmail.com>
2026-02-17 20:16:58 -0300

    feat: Implement sidebar menu with navigation links and footer in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonFooter } from '@ionic/angular/standalone';
 import { StatusBarService } from './core/services/status-bar.service';
+import { IonicModule } from "@ionic/angular";
+import { addIcons } from 'ionicons';
+import { homeOutline, swapHorizontalOutline, walletOutline, pieChartOutline, trophyOutline, logOutOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonFooter, IonicModule],
 })
 export class AppComponent {
   constructor(private statusBarService: StatusBarService) {
     this.statusBarService.init();
+    addIcons({ homeOutline, swapHorizontalOutline, walletOutline, pieChartOutline, trophyOutline, logOutOutline });
   }
 }
